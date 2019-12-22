2340efa2dc921e1696bc6b232e722a4224bb4ef6
Evgeniy <ev_malets@bk.ru>
2019-12-22 12:28:49 +0300

    --

--- transaction/operation.py
+++ transaction/operation.py
@@ -1,14 +1,35 @@
-from flask import request, Blueprint,flash,redirect,url_for,render_template,get_flashed_messages
+from flask import jsonify, request, Blueprint,flash,redirect,url_for,render_template,get_flashed_messages
 import random 
+import user
 bp = Blueprint('author',__name__,url_prefix='/author')
 
+#какой url?
+def place_order():
+
+  # принимаем объект корзины
+  buy_content = user.get_basket_data()
+
+  # ждем заполнения карты и подтверждения 
+  card_entry()
+
+  # true - удаляем(хотя надо перенести в колекцию "на обработку") объект корзины
+  # сообщае что все ок
+  # чистим
+  # возращаем на главную страницу
+
+  # false - забываем корзину(чистим)
+  # возращаем на главную страницу
+
+  # если ошибка не правильно заполнена карта то во зращаем на карту
+  #
 @bp.route('/card_entry/',method = ['POST'])
 def card_entry():
   if request.method == 'POST':
-    nomber_card = request.form['nomber_card']
-    holder = request.form['holder']
-    per_of_valid = request.form['per_of_valid']
-    cvv = request.form['cvv']
+    data = request.get_json(force=True)
+    nomber_card = data['nomber_card']
+    holder = data['holder']
+    per_of_valid = data['per_of_valid']
+    cvv = data['cvv']
     error = None
     if not nomber_card:
       error = "Не введен номер карты"
@@ -28,13 +49,18 @@ def card_entry():
         #транзакция подтверждена
         nomber_order= random.randint(000,999) 
         flash('Ваш заказ {} оформлен'.format(nomber_order))
-        return redirect(url_for('main_page'))
+        order = user.get_basket
+        
+        orders = database.get_db_connection()[database.COLLECTION_NAME]
+        orders.insert(order.get_order_data())
+        return jsonify(order = True,credentials = credentials, messages = get_flashed_messages())
+
+        
       else:
         #вернуться на заполнение карты
         flash('Отмена')#возможно не нужно
-        return render_template('auth/card_entry.html', credentials = credentials , messages = get_flashed_messages)
-        #возможно ошибки        ^^^^ 
-
+        return jsonify(order = False,credentials = credentials, messages = get_flashed_messages())
+        
 # 
 # scanf credit cart data
 # 
