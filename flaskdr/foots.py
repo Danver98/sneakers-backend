8fd1b6fb84b6f12488c1ba85b40bd1d2b896debe
Dmitriy <dmitriy.kononec@mail.ru>
2019-12-24 21:53:19 +0300

    'clear'

--- flaskdr/foots.py
+++
@@ -1,9 +0,0 @@
-class FootList:
-    def __init__(self):
-        self.foot_list = []
-
-    def add_foot(self, foot):
-        self.foot_list.append(foot)
-    
-    def get_all(self):
-        return self.foot_list
