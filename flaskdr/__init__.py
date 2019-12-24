e519aff22c0d3de9c8f22bfbcdb5a5f7db0124fe
Dmitriy <dmitriy.kononec@mail.ru>
2019-12-24 21:56:36 +0300

    'actualy'

---
+++ flaskdr/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask , request , render_template , Response , jsonify
+from flask_cors import CORS
+from instance.config import *
+from flaskdr.custom_response import CustomResponse
+from pymongo.errors import ConnectionFailure
+from werkzeug.exceptions import HTTPException, InternalServerError
+import pymongo, os
+SECRET_KEY = "*F-JaNdRgUkXp2s5v8y/B?E(H+KbPeSh"
+CONNECTION_PASSWORD = "C4pyEOgx7lD1dnce"
+ADMIN_NAME = " "
+DATABASE_URI = "mongodb+srv://danver98:{}@cluster0-nsbea.mongodb.net/test?retryWrites=true&w=majority".format(CONNECTION_PASSWORD)
+#DATABASE_URI = "mongodb+srv://dbDevelepner:54321@cluster0-dbjfp.azure.mongodb.net/test?retryWrites=true&w=majority"
+# добавить @login_required для других страниц
+
+def create_app(test_config = None,debug_config = True ,instance_relative_config = False):
+    app = Flask(__name__)
+    app.response_class = CustomResponse
+    if debug_config:
+        app.config.from_mapping(
+            SECRET_KEY = SECRET_KEY,
+            DATABASE_URI = DATABASE_URI
+        )
+    else:
+        app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") or SECRET_KEY
+        app.config.from_pyfile('production-config.py')
+    CORS(app)
+    #app.config.from_object(Configuration())
+    from . import database
+    # если @app.teardown_appcontext не используется
+    database.init_db(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import catalog
+    app.register_blueprint(catalog.app_catalog)
+    
+    from . import cart
+    app.register_blueprint(cart.ca)
+
+    @app.route('/')
+    def main_page():
+        return jsonify(success = True,messages="This is app main page") 
+    
+    @app.errorhandler(HTTPException)
+    def handle_exception(e):
+        if isinstance(e,InternalServerError):
+            return jsonify(error = -1 , messages = "Упс,произошла ошибка на сервере. Попробуйте выполнить действие ещё раз")
+        return jsonify(error = -2 , messages = "Упс,произошла ошибка при передаче данных. Попробуйте ещё раз")
+       
+    @app.errorhandler(ConnectionFailure)
+    def database_exception(e):
+        jsonify(error = -3 , messages = "Не удаётся выполнить запрос к базе данных. Попробуйте ещё раз")
+     
+    return app
+
---
+++ flaskdr/user.py
@@ -0,0 +1,66 @@
+from datetime import datetime , date
+from flaskdr.cart import Cart
+from werkzeug.security import check_password_hash, generate_password_hash
+import pymongo
+class User(object):
+    def __init__(self,first_name,last_name,birth_date,phone, email,password , cart = None):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        if isinstance(birth_date,str):        
+            self.__birth_date = datetime.strptime(birth_date,"%Y-%m-%d")
+        else:
+            self.__birth_date = birth_date
+        self.__phone = phone
+        self.__email = email
+        self.__password = generate_password_hash(password)
+        if cart is None:
+            self.__cart = Cart()
+        else:
+            self.__cart = cart
+
+    def _set_date(self,birth_date):
+        if isinstance(birth_date, date):
+            return birth_date     
+        elems = birth_date.split("-")
+        if len(elems) != 3:
+            raise Exception
+        return date(int(elems[0]) , int(elems[1]) , int(elems[2]))
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @property
+    def birth_date(self):
+        return self.__birth_date
+    
+    @property
+    def phone(self):
+        return self.__phone
+    
+    @property
+    def email(self):
+        return self.__email
+
+    @property
+    def cart(self):
+        return self.__cart   
+
+    def get_user_data(self):
+        return {"first_name" : self.__first_name , "last_name": self.__last_name , "birth_date": self.__birth_date, "phone": self.__phone , "email": self.__email , "password": self.__password}
+    
+    def get_user_data_no_passwd(self):
+        return {"first_name" : self.__first_name , "last_name": self.__last_name , "birth_date": self.__birth_date, "phone": self.__phone , "email": self.__email}
+
+    def get_user_data_with_cart(self):
+        return {"first_name" : self.__first_name , "last_name": self.__last_name , "birth_date": self.__birth_date, "phone": self.__phone , "email": self.__email , "cart":self.__cart} 
+
+    @classmethod
+    def convert_from_doc(cls,doc):
+        return User(doc['first_name'],doc['last_name'],doc['birth_date'],doc['phone'],doc['email'] ,doc['password'])
+        
+
