46c3e0dcb72f14d6141d18275bd5ee26d1558930
Dmitriy <dmitriy.kononec@mail.ru>
2019-12-24 21:22:18 +0300

    'master_test'

---
+++ flaskdr/custom_response.py
@@ -0,0 +1,12 @@
+from flask import Response , jsonify
+
+class CustomResponse(Response):
+    @classmethod
+    def force_type(cls,value,environ = None):
+        if isinstance(value,dict):
+            value = jsonify(value)
+        return super(CustomResponse ,cls).force_type(value,environ)
+
+class CustomDatabaseException(Exception):
+    def __init__(self):
+        super().__init__()
